3eefff55dcf72f3c74ba06d4c79d976eb2543155
John Louis Rumingan <jmrumingan@gmail.com>
2020-01-11 14:27:22 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,6 @@
 class StudentsController < ApplicationController
+
+  before_action :set_student, only: :show 
   
   def index
     @students = Student.all
@@ -7,5 +9,24 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+
+  def new 
+
+    #shows the new student erb page 
+  end 
+
+  def create 
+
+    @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    
+    session[:new_form_params] = params.inspect 
+    redirect_to new_student_path 
+  end 
+
+  private 
+    def set_student 
+
+      @student = Student.find(params[:id])
+    end 
 
 end
